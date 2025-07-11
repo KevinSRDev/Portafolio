fac4ea4b50c145676a50b405918b575ba0b22844
KevinSRDev <kevin.s.romero.r06@gmail.com>
2025-07-11 14:34:41 -0500

    Pequeñas modificacones

--- KR_Designer_Jorge/app.js
+++ KR_Designer_Jorge/app.js
@@ -1,11 +1,12 @@
 const colorSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchColor(e) {
-    if(e.target.checked){
+    if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-    } else{
+    } else {
         document.documentElement.setAttribute('data-theme', 'light');
     }
 }
 
 colorSwitch.addEventListener('change', switchColor, false);
+
