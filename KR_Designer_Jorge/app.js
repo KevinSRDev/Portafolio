bfd2ba2a43e6d0cb0d3d542c923bda9c9c487825
KevinSRDev <kevin.s.romero.r06@gmail.com>
2025-07-12 15:57:03 -0500

    Cambios en la section sobre mi

--- KR_Designer_Jorge/app.js
+++ KR_Designer_Jorge/app.js
@@ -1,12 +1,13 @@
-const colorSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const colorSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
 
 function switchColor(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
 }
 
-colorSwitch.addEventListener('change', switchColor, false);
-
+colorSwitch.addEventListener("change", switchColor, false);
